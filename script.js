fad04231ac12ff232442374adf6aafd0a30b90eb
heliogustavo <jacomehelioiq@gmail.com>
2022-10-17 17:08:52 -0400

    Versão Original

--- script.js
+++ script.js
@@ -1,4 +1,12 @@
-totalSelecionado = 0;
+let totalSelecionado = 0;
+let comidaSelecionada="";
+let bebidaSelecionada="";
+let sobremesaSelecionada="";
+let total;
+let texto;
+let precoComida=0;
+let precoBebida=0;
+let precoSobremesa=0;
 
 function marcaComida(borda1) {
     const marcado = document.querySelector('.comida .bordaVerde');
@@ -10,9 +18,13 @@ function marcaComida(borda1) {
       }
        if(borda1 !==marcado){
         borda1.classList.toggle('bordaVerde');
-        check.classList.add('corDoCheck')
+        check.classList.toggle('corDoCheck');
         totalSelecionado = totalSelecionado + 1;
-      }
+        comidaSelecionada = borda1.querySelector('.Prato').innerHTML;
+        let comidaPreco = borda1.querySelector('.h3valor').innerHTML.replace('R$','');
+        precoComida = Number(comidaPreco.replace(',','.'))
+
+     }
       ativarBotao();
 }
 function marcaBebida(borda2) {
@@ -25,6 +37,9 @@ function marcaBebida(borda2) {
        if(borda2 !==marcado){
         borda2.classList.toggle('bordaVerde');
         totalSelecionado = totalSelecionado + 1;
+        bebidaSelecionada = borda2.querySelector('.bebida').innerHTML;
+        let bebidaPreco = borda2.querySelector('.h3valor').innerHTML.replace('R$','');
+        precoBebida = Number(bebidaPreco.replace(',','.'))
 
       }
       ativarBotao();
@@ -39,6 +54,9 @@ function marcaSobremesa(borda3) {
       if(borda3 !==marcado){
          borda3.classList.toggle('bordaVerde');
          totalSelecionado = totalSelecionado + 1;
+         sobremesaSelecionada = borda3.querySelector('.doce').innerHTML;
+         let sobremesaPreco = borda3.querySelector('.h3valor').innerHTML.replace('R$','');
+        precoSobremesa = Number(sobremesaPreco.replace(',','.'))
       }
          ativarBotao();
 }
@@ -46,7 +64,7 @@ function marcaSobremesa(borda3) {
 function ativarBotao(){
     const novoBotao = document.querySelector('.botao-fechar-pedido')
     novoBotao.setAttribute('disabled', 'disabled'); 
-
+    
     if(totalSelecionado == 3) {
         novoBotao.innerHTML = 'Fechar Pedido';
         novoBotao.classList.add('botaoVerde')
@@ -57,75 +75,19 @@ function ativarBotao(){
         novoBotao.innerHTML = 'Selecione os 3 itens para fechar o Pedido';
         novoBotao.classList.remove('botaoVerde')
             novoBotao.setAttribute('disabled', 'disabled');
-
     }
-
-
-}
-// function botaoAtivo(){
-//     const botaoFinal = document.querySelector('.botao-fechar-pedido');
-//     let totalBordas = document.querySelectorAll('.bordaVerde');
-
-//     if (totalBordas.length == 3) {
-//         botaoFinal.setAttribute=; 
-//       } else {
-//         botaoFinal.disabled = false;
-// }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/*function removerBorda(item){
-    item.classList.remove("bordastyle")
 }
 
-function marcaItem() {
-    let comida= document.querySelector(".comida");
-    let filhosComida = Array.from(comida.children);
-    filhosComida.forEach(removerBorda);
-    var bordaVerde = document.querySelector(".ceviche");
-    bordaVerde.classList.toggle("bordastyle");
-    
 
+function whatsApp() {
+    total= precoComida + precoBebida + precoSobremesa;
+    let totalFinal = toString(total);
+    let texto = `Olá, gostaria de fazer o pedido:
+    - Prato: ${comidaSelecionada}
+    - Bebida: ${bebidaSelecionada}
+    - Sobremesa: ${sobremesaSelecionada}
+    Total: R$ ${total}`
 
-
-
-
-
-    function marcaItem() {
-    const botaoSelecionadoAnteriormente = document.querySelector('.selecionado');
-    console.log(null);
-    opcoes.classList.toggle('bordaVerde');
-
-        if(botaoSelecionadoAnteriormente !== null){
-            botaoSelecionadoAnteriormente.classList.remove(".selecionado")
-        }
-        else{
-            const borda
-        }
-}
-
-  }*/
-
-
-
+    let textoCodificado = encodeURIComponent(texto);
+    window.open("https://wa.me/5592986201808?text=" + textoCodificado);
+  }
